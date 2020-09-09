273c3fcfca82b86035f712710f41a5c59a0dedf7
Tom Hermans <tomhermans@gmail.com>
2020-09-09 10:50:44 +0200

    Update tomhermans.js

--- _data/sites/tomhermans.js
+++ _data/sites/tomhermans.js
@@ -1,18 +1,21 @@
 module.exports = {
-	name: "tomhermans.com", // optional, falls back to object key
-	description: "Zach’s Personal web site",
+	name: "Tom Hermans", // optional, falls back to object key
+	description: "Some of my own sites",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	// skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
-		frequency: 60 * 23, // 23 hours
+		frequency: 60 * 23, // 24 hours
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
 		// Use "site" if sites are all on the same origin and share assets.
-		freshChrome: "site"
+		freshChrome: "run"
 	},
 	urls: [
+		"https://www.11ty.dev/",
+		"https://www.gatsbyjs.com/",
+		
 		"http://www.tomhermans.com/",
 		"https://vibrant-benz-d6c5e1.netlify.app/",
 		"https://tomhermans-cv.netlify.app/",
